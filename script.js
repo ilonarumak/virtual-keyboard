c380a1d5329cf7dc321e8fb57cc140c0f8e5b3fb
IlonaSoroko <ilona.rumak7@gmail.com>
2020-04-07 23:04:29 +0300

    feat: add design

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let input = document.createElement('textarea');
 let keyboardCodesEng = [96, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 113, 119, 101, 114, 116, 121, 117, 105, 111, 112, 91, 93, 92, 97, 115, 100, 102, 103, 104, 106, 107, 108, 59, 39, 122, 120, 99, 118, 98, 110, 109, 44, 46, 47];
 let keyboardCodesRus = [1105, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 45, 61, 1081, 1094, 1091, 1082, 1077, 1085, 1075, 1096, 1097, 1079, 1093, 1098, 92, 1092, 1099, 1074, 1072, 1087, 1088, 1086, 1083, 1076, 1078, 1101, 1103, 1095, 1089, 1084, 1080, 1090, 1100, 1073, 1102, 46];
 let keyboardWhich = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191];
+let keyboardCodesShift = [126, 33, 64, 35, 36, 37, 94, 38, 42, 40, 41, 95, 43];
 let firstRow = document.createElement('div');
 let secondRow = document.createElement('div');
 let thirdRow = document.createElement('div');
@@ -45,7 +46,7 @@ backspace.innerHTML = 'Backspace';
 del.innerHTML = 'Delete';
 enter.innerHTML = 'Enter';
 tab.innerHTML = 'Tab';
-capsLk.innerHTML = 'CapsLk';
+capsLk.innerHTML = 'CapsLock';
 shiftLeft.innerHTML = 'Shift';
 ctrlLeft.innerHTML = 'Ctrl';
 win.innerHTML = 'Win';
@@ -55,7 +56,7 @@ down.innerHTML = '↓';
 left.innerHTML = '←';
 right.innerHTML = '→';
 space.innerHTML = 'Space';
-description.innerHTML = 'Клавиатура создана в операционной системе <b>Windows</b>.<br> Переключение языка: <b>Shift + Alt</b>';
+description.innerHTML = 'Клавиатура создана в операционной системе <b>Windows</b>.<br> Переключение языка: <b>Ctrl (левый) + Alt (левый)</b>';
 
 keyboard.after(description);
 
@@ -96,18 +97,28 @@ let altRight = altLeft.cloneNode(true);
 /* создание кнопок */
 
 let keyboardKeys = (document.cookie == 'language=rus') ? init(keyboardCodesRus) : init(keyboardCodesEng);
-let keyboardLetters = [];
+let keyboardKeysRegular = [];
 for (let i = 0; i < keyboardKeys.length; i++) {
    if (keyboardKeys[i].classList.length < 2) {
-      keyboardLetters.push(keyboardKeys[i]);
+      keyboardKeysRegular.push(keyboardKeys[i]);
    }
+};
+let keyboardLetters = [];
+for (let i = 13; i < keyboardKeysRegular.length; i++) {
+   keyboardLetters.push(keyboardKeysRegular[i]);
+}
+
+/* снятие выделения текста при двойном нажатии ЛКМ */
+
+keyboard.onmousedown = keyboard.onselectstart = function () {
+   return false;
 };
 
 /* нажатие на кнопку виртуальной клавиатуры */
 
 keyboardKeys.forEach(elem => {
    elem.addEventListener('mousedown', function () {
-      let cursorPos = getCursorPosition(input);
+      let cursorPos = getCaret(input);
       switch (elem.innerHTML) {
          case 'Space':
             input.innerHTML += ' ';
@@ -116,24 +127,31 @@ keyboardKeys.forEach(elem => {
             input.innerHTML += '\r\n';
             break;
          case 'Tab':
+            toActive(elem);
             input.innerHTML += '    ';
             break;
          case 'Alt':
+            toActive(elem);
+            break;
          case 'Ctrl':
+            elem.classList.toggle('keyboard__keys_active');
+            toDeactiveByClick(elem);
+            toDeactiveByPress(elem);
+            break;
          case 'Shift':
+            toActiveCapsOrShift(elem);
+            changeFirstRow();
+            toDeactiveByClick(elem);
+            toDeactiveByPress(elem);
             break;
          case 'Backspace':
-            toBackspace(input, cursorPos);
+            toBackspace();
             break;
-         case 'CapsLk':
-            keyboardLetters.forEach(elem => {
-               elem.classList.toggle('keyboard__keys_upperCase');
-            });
+         case 'CapsLock':
+            toActiveCapsOrShift(elem);
             break;
          case 'Delete':
             toDelete(input, cursorPos);
-            break;
-         case 'Win':
             break;
          default:
             if (document.querySelectorAll('.keyboard__keys_upperCase').length === 0) {
@@ -147,40 +165,58 @@ keyboardKeys.forEach(elem => {
    });
 
    elem.addEventListener('mouseup', function () {
-      elem.classList.remove('keyboard__keys_active');
-      cursorPos = getCursorPosition(input);
+      if (elem == shiftLeft || elem == shiftRight || elem == capsLk || elem == ctrlLeft) {
+         return;
+      }
+      else {
+         elem.classList.remove('keyboard__keys_active');
+         if (ctrlLeft.classList.contains('keyboard__keys_active') && elem == altLeft) {
+            elem.classList.add('keyboard__keys_active');
+            checkActiveKeys(ctrlLeft, altLeft);
+            keyboardKeys.forEach(key =>
+               key.classList.remove('keyboard__keys_active'));
+         }
+      }
    })
 });
 
 /* нажатие на кнопку физической клавиатуры */
 
+document.addEventListener('keypress', function (event) {
+
+})
+
 document.addEventListener('keydown', function (event) {
 
    /* подсветка нужной клавиши */
-   
+
    if (event.which == 16) {
       let shift = (event.location == 1) ? shiftLeft : shiftRight;
-      toActive(shift);
-      checkActiveKeys(shiftLeft, altLeft);
+      shift.classList.add('keyboard__keys_active');
+      keyboardKeysRegular.forEach(elem => {
+         elem.classList.add('keyboard__keys_upperCase');
+      });
+      changeFirstRow();
       return;
    }
    if (event.which == 18) {
       let alt = (event.location == 1) ? altLeft : altRight;
       toActive(alt);
       event.preventDefault();
-      checkActiveKeys(shiftLeft, altLeft);
+      checkActiveKeys(ctrlLeft, altLeft);
       return;
    }
    if (event.which == 17) {
       let ctrl = (event.location == 1) ? ctrlLeft : ctrlRight;
-      toActive(ctrl);
+      ctrl.classList.add('keyboard__keys_active');
       event.preventDefault();
+      checkActiveKeys(ctrlLeft, altLeft);
       return;
    }
    toActive(document.querySelector('.keyboard__keys[data="' + event.which + '"]'));
 
    /* вывод при нажатии клавиши */
-   
+
    switch (event.which) {
       case 13:
          input.innerHTML += '\r\n';
@@ -193,9 +229,7 @@ document.addEventListener('keydown', function (event) {
          input.innerHTML += ' ';
          break;
       case 20:
-         keyboardLetters.forEach(elem => {
-            elem.classList.toggle('keyboard__keys_upperCase');
-         })
+         toActiveCapsOrShift(capsLk);
          event.preventDefault();
          break;
       case 8:
@@ -212,11 +246,23 @@ document.addEventListener('keydown', function (event) {
    }
 });
 
-document.addEventListener('keyup', function toDeactive() {
-   keyboardKeys.forEach(elem => {
-      elem.classList.remove('keyboard__keys_active');
+document.addEventListener('keyup', function toDeactive(event) {
+         if (event.key == 'CapsLock') {
+            return;
+         }
+         else if (event.key == 'Shift') {
+      //       changeFirstRow();
+            shiftLeft.classList.remove('keyboard__keys_active');
+            shiftRight.classList.remove('keyboard__keys_active');
+            keyboardKeysRegular.forEach(key =>
+               key.classList.remove('keyboard__keys_upperCase'));
+         }
+         else {
+            keyboardKeys.forEach(elem => 
+      elem.classList.remove('keyboard__keys_active')
+            )}
    });
-});
+   // });
 
 /* функция создания клавиатуры */
 
@@ -262,32 +308,144 @@ function changeKeyboardKeys(elem, codes, i) {
    elem.innerHTML = String.fromCharCode(codes[i]);
 }
 
-function toActive(elem) {
-   elem.classList.add('keyboard__keys_active');
-}
-
-function getCursorPosition(ctrl) {
-   var CaretPos = 0;
-   if (document.selection) {
-      ctrl.focus();
-      var Sel = document.selection.createRange();
-      Sel.moveStart('character', -ctrl.value.length);
-      CaretPos = Sel.text.length;
-   } else if (ctrl.selectionStart || ctrl.selectionStart == '0') {
-      CaretPos = ctrl.selectionStart;
-   }
-   return CaretPos;
-}
-
-function toBackspace(input, cursorPos) {
-   let inputValue = input.innerHTML.split('');
-   if (cursorPos == inputValue.length - 1) {
-      inputValue.pop();
+function changeFirstRow() {
+   if (keyboardKeysRegular[0].innerHTML == '~') {
+      if (keyboardKeysRegular[13].innerHTML === 'q') {
+         changeFirstRowKeys(keyboardCodesEng);
+      }
+      else {
+         changeFirstRowKeys(keyboardCodesRus);
+      }
    }
    else {
-      inputValue.splice(cursorPos - 1, 1);
+      changeFirstRowKeys(keyboardCodesShift);
    }
-   input.innerHTML = inputValue.join('');
+}
+
+function changeFirstRowKeys(codes) {
+   let firstRowKeys = document.querySelectorAll('.first-row > div');
+   for (let i = 0; i < firstRowKeys.length - 1; i++) {
+      changeKeyboardKeys(firstRowKeys[i], codes, i);
+   }
+}
+
+function toActive(elem) {
+   if (elem == shiftLeft || elem == shiftRight || elem == capsLk || elem == ctrlLeft) {
+      return;
+   }
+   else {
+      elem.classList.add('keyboard__keys_active');
+   }
+}
+
+function toActiveCapsOrShift(elem) {
+   elem.classList.toggle('keyboard__keys_active');
+   keyboardLetters.forEach(key => {
+      key.classList.toggle('keyboard__keys_upperCase');
+   })
+}
+
+function toDeactiveByClick(elem) {
+   keyboardLetters.forEach(key =>
+      key.onclick = function () {
+         elem.classList.remove('keyboard__keys_active');
+         keyboardKeysRegular.forEach(key =>
+            key.classList.remove('keyboard__keys_upperCase'));
+         if (elem == shiftLeft) {
+            changeFirstRow();
+         }
+      })
+   toDeactiveShift(shiftLeft, shiftRight);
+   toDeactiveShift(shiftRight, shiftLeft);
+}
+
+function toDeactiveByPress(elem) {
+   document.onkeypress = function () {
+      keyboardKeysRegular.forEach(key =>
+         key.classList.remove('keyboard__keys_upperCase'));
+      elem.classList.remove('keyboard__keys_active');
+      if (elem == shiftLeft) {
+         changeFirstRow();
+      }
+   }
+}
+
+function toDeactiveShift(elem1, elem2) {
+   if (elem1.classList.contains('keyboard__keys_active')) {
+      elem2.onclick = () => {
+         elem1.classList.remove('keyboard__keys_active');
+         elem2.classList.remove('keyboard__keys_active');
+      }
+   }
+}
+
+// function getCursorPosition(ctrl) {
+//    let CaretPos = 0;
+//    if (document.selection) {
+//       ctrl.focus();
+//       let Sel = document.selection.createRange();
+//       Sel.moveStart('character', -ctrl.value.length);
+//       CaretPos = Sel.text.length;
+//    } else if (ctrl.selectionStart || ctrl.selectionStart == '0') {
+//       CaretPos = ctrl.selectionStart;
+//    }
+//    return CaretPos;
+// }
+
+// function toBackspace(input, cursorPos) {
+//    let inputValue = input.innerHTML.split('');
+//    if (cursorPos == inputValue.length - 1) {
+//       inputValue.pop();
+//    }
+//    else {
+//       inputValue.splice(cursorPos - 1, 1);
+//    }
+//    input.innerHTML = inputValue.join('');
+// }
+
+function getCaret(el) {
+   if (el.selectionStart) {
+      return el.selectionStart;
+   } else if (document.selection) {
+      el.focus();
+
+      let r = document.selection.createRange();
+      if (r == null) {
+         return 0;
+      }
+
+      let re = el.createTextRange(),
+         rc = re.duplicate();
+      re.moveToBookmark(r.getBookmark());
+      rc.setEndPoint('EndToStart', re);
+
+      return rc.text.length;
+   }
+   return 0;
+}
+
+function resetCursor(txtElement, currentPos) {
+   if (txtElement.setSelectionRange) {
+      txtElement.focus();
+      txtElement.setSelectionRange(currentPos, currentPos);
+   } else if (txtElement.createTextRange) {
+      let range = txtElement.createTextRange();
+      range.moveStart('character', currentPos);
+      range.select();
+   }
+}
+
+function toBackspace() {
+   let textarea = document.querySelector('.input');
+   let currentPos = getCaret(textarea);
+   let text = textarea.value;
+
+   let backSpace = text.substr(0, currentPos - 1) + text.substr(currentPos, text.length);
+
+   textarea.value = backSpace;
+
+   resetCursor(textarea, currentPos - 1);
+
 }
 
 function toDelete(input, cursorPos) {
@@ -300,21 +458,26 @@ function toDelete(input, cursorPos) {
 
 function checkActiveKeys(a, b) {
    if (a.classList.contains('keyboard__keys_active') && b.classList.contains('keyboard__keys_active')) {
-      keyboardLetters.forEach(key =>
-         key.classList.remove('keyboard__keys_active'));
-      if (keyboardLetters[0].innerHTML == '`') {
-         for (let i = 0; i < keyboardLetters.length; i++) {
-            changeKeyboardKeys(keyboardLetters[i], keyboardCodesRus, i);
-            document.cookie = 'language=rus';
-            console.log(document.cookie);
+      keyboardKeysRegular.forEach(key =>
+         key.classList.remove('keyboard__keys_active', 'keyboard__keys_upperCase'));
+
+      if (keyboardKeysRegular[0].innerHTML == '`') {
+         for (let i = 0; i < keyboardKeysRegular.length; i++) {
+            changeKeyboardKeys(keyboardKeysRegular[i], keyboardCodesRus, i);
          }
+         document.cookie = "language=rus";
+         console.log(document.cookie);
       }
       else {
-         for (let i = 0; i < keyboardLetters.length; i++) {
-            changeKeyboardKeys(keyboardLetters[i], keyboardCodesEng, i);
+         for (let i = 0; i < keyboardKeysRegular.length; i++) {
+            changeKeyboardKeys(keyboardKeysRegular[i], keyboardCodesEng, i);
             document.cookie = 'language=eng';
             console.log(document.cookie);
          }
       }
+      shiftLeft.classList.remove('keyboard__keys_active');
+      capsLk.classList.remove('keyboard__keys_active');
+
    }
 }
+
